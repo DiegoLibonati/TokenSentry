ce82faae4b1ac60bc428582d5c85f0936733711a
Die <dlibonati@leafnoise.io>
2025-10-12 20:34:40 -0300

    feat: separate command fn from register fn

--- src/commands/aliveCommand.ts
+++ src/commands/aliveCommand.ts
@@ -1,7 +1,9 @@
 import * as vscode from "vscode";
 
+const aliveCommand = () => {
+  vscode.window.showInformationMessage("Hello world from TokenSentry.");
+};
+
 export const registerAliveCommand = () => {
-  return vscode.commands.registerCommand("tokensentry.alive", () => {
-    vscode.window.showInformationMessage("Hello world from TokenSentry.");
-  });
+  return vscode.commands.registerCommand("tokensentry.alive", aliveCommand);
 };
--- src/commands/checkFilesCommand.ts
+++ src/commands/checkFilesCommand.ts
@@ -8,54 +8,56 @@ import { getFullPathFile } from "@src/helpers/getFullPathFile";
 
 import { GIT_FOLDER_NAME } from "@src/constants/vars";
 
-export const registerCheckFilesCommand = () => {
-  return vscode.commands.registerCommand("tokensentry.checkFiles", async () => {
-    const workspaceFolders = vscode.workspace.workspaceFolders;
-    const patterns = loadPatterns();
+const checkFiles = async () => {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  const patterns = loadPatterns();
 
-    if (workspaceFolders?.length === 0) {
-      return vscode.window.showErrorMessage(
-        "You are not standing on any folder."
-      );
-    }
+  if (workspaceFolders?.length === 0) {
+    return vscode.window.showErrorMessage(
+      "You are not standing on any folder."
+    );
+  }
 
-    const currentDir = workspaceFolders![0].uri.fsPath;
+  const currentDir = workspaceFolders![0].uri.fsPath;
 
-    const nameDirs = getDirectoriesNameByPath(currentDir);
+  const nameDirs = getDirectoriesNameByPath(currentDir);
 
-    if (!nameDirs.includes(GIT_FOLDER_NAME)) {
-      return vscode.window.showErrorMessage(
-        "Git is not initialized in this folder."
-      );
-    }
+  if (!nameDirs.includes(GIT_FOLDER_NAME)) {
+    return vscode.window.showErrorMessage(
+      "Git is not initialized in this folder."
+    );
+  }
 
-    vscode.window.showInformationMessage("Checking for tokens...");
+  vscode.window.showInformationMessage("Checking for tokens...");
 
-    const git = simpleGit({ baseDir: currentDir });
+  const git = simpleGit({ baseDir: currentDir });
 
-    const files = await git.diff(["--cached", "--name-only"]);
-    const filenames = files.split("\n").filter((f) => f);
+  const files = await git.diff(["--cached", "--name-only"]);
+  const filenames = files.split("\n").filter((f) => f);
 
-    if (filenames.length === 0) {
-      return vscode.window.showErrorMessage(
-        "There are no files added to commit."
-      );
-    }
+  if (filenames.length === 0) {
+    return vscode.window.showErrorMessage(
+      "There are no files added to commit."
+    );
+  }
 
-    for (const filename of filenames) {
-      const filePath = getFullPathFile(currentDir, filename);
-      const content = fs.readFileSync(filePath, "utf8");
+  for (const filename of filenames) {
+    const filePath = getFullPathFile(currentDir, filename);
+    const content = fs.readFileSync(filePath, "utf8");
 
-      for (const [patterName, pattern] of Object.entries(patterns)) {
-        if (pattern.test(content)) {
-          vscode.window.showWarningMessage(
-            `Potential token detected in ${filename} with pattern: ${patterName}.`
-          );
-          break;
-        }
+    for (const [patterName, pattern] of Object.entries(patterns)) {
+      if (pattern.test(content)) {
+        vscode.window.showWarningMessage(
+          `Potential token detected in ${filename} with pattern: ${patterName}.`
+        );
+        break;
       }
     }
+  }
 
-    vscode.window.showInformationMessage("Analysis successfully completed.");
-  });
+  vscode.window.showInformationMessage("Analysis successfully completed.");
+};
+
+export const registerCheckFilesCommand = () => {
+  return vscode.commands.registerCommand("tokensentry.checkFiles", checkFiles);
 };
